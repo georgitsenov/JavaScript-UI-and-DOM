e1eadd80b1dc75b71135fe231c7a4be8746e5869
Georgi Tsenov <georgi.tsenov.90@gmail.com>
2017-11-15 04:41:20 +0200

    DOM Operations Homework

--- Topics/06. DOM Operations/homework/tasks/task-1.js
+++ Topics/06. DOM Operations/homework/tasks/task-1.js
@@ -20,5 +20,31 @@ Create a function that takes an id or DOM element and an array of contents
 module.exports = function () {
 
   return function (element, contents) {
+  	let theElement,
+  		docFragment = document.createDocumentFragment();
+
+  	if(typeof element === 'string') {
+  		theElement = document.getElementById(element);
+  		if(!theElement) {
+  			throw new Error('The ID does not select anything!');
+  		}
+  	} else if(element instanceof HTMLElement) {
+  		theElement = element;
+  	} else {
+  		throw new Error('No valid ID or HTMLElement');
+  	}
+
+  	contents.forEach(item => {
+  		if(typeof item != 'string' && typeof item != 'number') {
+  			throw new Error('The content is not strings or numbers');
+  		}
+  		let content = document.createElement('div')
+  		content.innerHTML = item;
+  		docFragment.appendChild(content);
+  	});
+
+  	theElement.innerHTML = '';
+  	theElement.appendChild(docFragment);
+
   };
 };
